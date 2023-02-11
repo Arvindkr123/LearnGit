46a5919fec24c08edb94cd4d77d438a3a06fdfd7
Arvind <thakurarvindkr10@gmail.com>
2023-02-11 10:39:26 +0530

    I have added java script file right now and modified the some html file

---
+++ main.js
@@ -0,0 +1,30 @@
+// Examine the document object//
+
+// console.log("hello World");
+// const headerTitle = document.getElementById("header-title");
+// console.log(typeof headerTitle);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// console.log(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent= "hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// get element by Id
+// console.log(document.getElementById("header-title"));
+const headerTitle = document.getElementById("header-title");
+console.log(headerTitle);
+headerTitle.textContent = "HELLO";
+headerTitle.innerText = "Good Bye";
+
+
+

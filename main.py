bed2254ffd182a1cb87da71cb53fee6bfd308341
Arvind <thakurarvindkr10@gmail.com>
2023-02-11 06:37:32 +0530

    I have comment some line of code because file is python that's why i comment all lines of code

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-first line
-second line
-third line
-fourth line 
-five line
-six line 
-seven line
-Eight line
-Nine line 
-Ten line hello there eleven line of code
-public class Person{
-public static void main(String[] args){
-           System.out.println("hello World");
-    }
-}
-String s = "hello there";
-System.out.println(s);
-[D[D[D[D[A[A[C[B[C[[C[C[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+# first line
+# second line
+# third line
+# fourth line 
+# five line
+# six line 
+# seven line
+# Eight line
+# Nine line 
+# Ten line hello there eleven line of code
+# public class Person{
+# public static void main(String[] args){
+#            System.out.println("hello World");
+#     }
+# }
+# String s = "hello there";
+# System.out.println(s);
